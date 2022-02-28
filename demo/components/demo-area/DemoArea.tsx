d247eb33baaf1fe0f18826778fc455fd8c443c11
yuan tan <dyxtan@gmail.com>
2022-03-01 04:45:54 +1300

    add type file

--- demo/components/demo-area/DemoArea.tsx
+++ demo/components/demo-area/DemoArea.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import './demo-area.css'
+import { AnimateTraceBorder } from "../../../src/animate-trace-border";
 
 
 const DemoArea = () => {
 
+
+  return (
+    <div className="demo-area">
+
+      <div className="demo-item">
+
+      </div>
+    </div>
+  )
 }
 
 export default DemoArea;
