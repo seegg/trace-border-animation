4a17fd5b9029bc286cb3ed0f61b0c9babb392d4b
yuan tan <dyxtan@gmail.com>
2022-03-01 04:46:03 +1300

    add type file

---
+++ demo/type.ts
@@ -0,0 +1,13 @@
+export interface ITraceBorderProps {
+  children?: React.ReactNode,
+  borderWidth?: number,
+  borderRadius?: number,
+  borderColour?: string,
+  animationDuration?: number,
+  speed?: number,
+  borderStyle?: string,
+  squareWindow?: boolean,
+  inset?: boolean,
+  trigger?: string
+
+}
