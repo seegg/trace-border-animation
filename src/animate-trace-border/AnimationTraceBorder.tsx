60b6cc6482459ce6e6497768e3c2d959d0c496c5
yuan tan <dyxtan@gmail.com>
2022-03-02 03:18:03 +1300

    remove use state from trace border component

--- src/animate-trace-border/AnimationTraceBorder.tsx
+++ src/animate-trace-border/AnimationTraceBorder.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef } from 'react';
 import buildTraceFunctions from './traceBorder';
 import { initialiseBorderStyles } from './initialise-styles';
 import { assignStyling as resetBorderStyle } from './util';
