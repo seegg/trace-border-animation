c39068c7a6f402c1727eb51fcbd7872024b46a0d
yuan tan <dyxtan@gmail.com>
2022-03-02 17:05:38 +1300

    add headers

---
+++ demo/components/header/Header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './header.css';
+import { AnimateTraceBorder } from "../../../src/animate-trace-border";
+
+const Header = () => {
+
+  return (
+    <header className="nav-header">
+      <nav>
+        <AnimateTraceBorder animationDuration={0} borderWidth={2} inset borderColour="black">
+          <div className="nav-item">
+            <a href="https://github.com/seegg/trace-border-animation"><img src="https://raw.githubusercontent.com/seegg/seegg.github.io/main/images/GitHub-Mark-Light-32px.png" alt="github logo" /></a>
+          </div>
+        </AnimateTraceBorder>
+      </nav>
+    </header>
+  )
+
+};
+
+export default Header;
